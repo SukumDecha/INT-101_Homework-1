03f8b0b059f3dd7826531c6a96b2fcc84db9d4b5
SukumDecha <ninemaster12gt@gmail.com>
2023-09-20 22:52:12 +0700

    Initial commit

---
+++ src/main/java/work03/MyUtil.java
@@ -0,0 +1,11 @@
+package work03;
+
+public class MyUtil {
+    public static double calculateBMI(double height, double weight) {
+        return weight / (height * height);
+    }
+
+    public static double average(int v1, int v2, int v3) {
+        return (double) (v1 + v2 + v3) / 3;
+    }
+}
