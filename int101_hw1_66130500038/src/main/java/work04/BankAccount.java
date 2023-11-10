49fff7c9abd748b34e6950ca5ef55814027a6531
Teprawin Prueksachat <36534399+SukumDecha@users.noreply.github.com>
2023-11-10 16:22:56 +0700

    Add files via upload
    cleanup directory

---
+++ int101_hw1_66130500038/src/main/java/work04/BankAccount.java
@@ -0,0 +1,45 @@
+package work04;
+
+public class BankAccount {
+    private int number;
+    private Person owner;
+    private double balance;
+
+    public BankAccount(int number, Person owner) {
+        this.number = number;
+        this.owner = owner;
+    }
+    public Person getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double deposit(double amount) {
+        if(amount > 0) balance += amount;
+
+        return balance;
+    }
+
+    public double withdraw(double amount) {
+        if(amount > 0 && amount <= balance) balance -= amount;
+
+        return balance;
+    }
+
+    public void transfer(double amount, BankAccount account) {
+        if(amount > 0 && amount <= balance ) {
+            withdraw(amount);
+            account.deposit(amount);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "number=" + number +
+                '}';
+    }
+}
